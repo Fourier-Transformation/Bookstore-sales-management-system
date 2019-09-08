9f9d1508a7b740a3b899bf9f45b783f59952ea5c
laplace <3028281331@qq.com>
2019-09-08 11:05:38 +0800

    study var rules

--- server/app.py
+++ server/app.py
@@ -2,10 +2,26 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello')  # which URL can trigger function
 def hello_world():
     return 'hello,world!'
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')  # root
+def index():
+    return 'Index Page'
+
+
+@app.route('/user/<username>')  # variable rules: <variable name>
+def show_user_profile(username: str):
+    return 'User %s' % username
+
+
+# choose the type of variable: <converter:variable name>
+@app.route('/post/<int:post_id>')
+def show_post(post_id: int):
+    return 'Post %d' % post_id
+
+
+if __name__ == '__main__':  # ensure that it can't execute automaticly when importing
+    app.run(debug=True)  # debug model
