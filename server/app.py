4684b0d9513a0666c16ed70475ac6d67d84136e9
laplace <3028281331@qq.com>
2019-09-08 10:32:43 +0800

    test flask

--- server/app.py
+++ server/app.py
@@ -1,3 +1,11 @@
-print('Hello World')
+from flask import Flask
+app = Flask(__name__)
 
-print('laplace')
+
+@app.route('/')
+def hello_world():
+    return 'hello,world!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
