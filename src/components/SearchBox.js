04dc8515e955d64e85e7cff9f3d4e3cd06836303
loliimay <lolimay@lolimay.cn>
2019-09-25 21:17:22 +0800

    Add component SearchBox

---
+++ src/components/SearchBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default class SearchBox extends React.Component {
+    render() {
+        return (
+            <div className='search-box'>这是一个搜索框</div>
+        );
+    };
+}
