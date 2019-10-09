d1303c27285ca4b4994208b291c835589102bf44
oowsxq <oowsxq@users.noreply.github.com>
2019-10-09 15:22:01 +0800

    format

--- server/utils/config.py
+++ server/utils/config.py
@@ -1,13 +1,13 @@
-'''
+"""
 Load Configrations from ini file
-'''
+"""
 
 import configparser as _configparser
 
 def load(path, section):
-    '''
+    """
     load configrations by specifying config file path and the section
-    '''
+    """
     parser = _configparser.ConfigParser()
     parser.read(path, encoding='utf-8')
 
