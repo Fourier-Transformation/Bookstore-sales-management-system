2968ef6244b47e60ca1ff44e8e73fef0f955d228
oowsxq <oowsxq@users.noreply.github.com>
2019-10-10 23:58:16 +0800

    add feat & fix error

--- server/database/bsdb.py
+++ server/database/bsdb.py
@@ -2,13 +2,14 @@
 bookstore database instance
 """
 
-import utils.config as _config_parser
+from .utils import config as _config_parser
 from . import connector as _connector
 
 
 CONFIG_PATH = 'config.ini'
 
-class BSDB(object):
+
+class BookStoreDataBase(object):
     """
     support an interface with database without SQL
     THIS CLASS SHOULD NOT HAVE MORE THAN ONE INSTANCE
@@ -16,7 +17,8 @@ class BSDB(object):
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '_inst'):
-            cls._inst = super(BSDB, cls).__new__(cls, *args, **kwargs)
+            cls._inst = super(BookStoreDataBase, cls).__new__(
+                cls, *args, **kwargs)
         return cls._inst
 
     def __init__(self):
@@ -35,20 +37,41 @@ class BSDB(object):
             SELECT * FROM Books;
             '''
         sql_result = self._my_connect.execute_sql(sql_expr)
-        print(sql_result)
+        return sql_result
 
     def get_book_isbn(self, isbn: str) -> list:
         """
         return a list of book selected by ISBN
         """
-        pass
+        sql_expr = \
+            '''
+            SELECT * FROM Books WHERE isbn = '%s'
+            ''' % (isbn)
+        sql_result = self._my_connect.execute_sql(sql_expr)
+        return sql_result
 
     def get_book_name(self, name: str) -> list:
         """
         return a list of book selected by name
         """
-        pass
+        sql_expr = \
+            '''
+            SELECT * FROM Books WHERE name = '%s'
+            ''' % (name)
+        sql_result = self._my_connect.execute_sql(sql_expr)
+        return sql_result
 
-if __name__ == '__main__':
-    db = BSDB()
-    print(db.get_book_all())
+    def get_book_name_part(self, name: str) -> list:
+        """
+        return a list of book selected by %name%
+        """
+        sql_expr = \
+            '''
+            SELECT * FROM Books WHERE name = '%{0}%'
+            '''.format(name)
+        sql_result = self._my_connect.execute_sql(sql_expr)
+        return sql_result
+
+
+# the only instance of BSDB
+BOOKSTORE_DATABASE = BookStoreDataBase()
