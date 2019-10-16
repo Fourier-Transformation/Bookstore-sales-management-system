b994a3fee145e458689ce8676a3860f68cc4a1f4
laplace <3028281331@qq.com>
2019-10-16 16:21:27 +0800

    keys are changed to English

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, url_for, json, jsonify
 from proc import query
 
 app = Flask(__name__)
@@ -9,7 +9,8 @@ def index():
     """
     return index page
     """
-    return 'Welcome to our online bookstore!'
+    # return {'tips': '欢迎光临！'}
+    return '欢迎光临！'
 
 
 @app.route('/login/', methods=['get', 'post'])
@@ -26,7 +27,9 @@ def show_user_profile(username: str):
     return user's profile
     param username: the ID of users
     """
-    return 'User %s' % username
+    url = url_for('user', name=username)
+    print(url)
+    return redirect(url)
 
 
 @app.route('/books/')
@@ -34,18 +37,43 @@ def show_books():
     """
     show a list of all the books
     """
-    return {'books': query.get_book_list()}
+    result = {'books': query.get_book_list()}
+    return jsonify(result)
 
 
-# @app.route('/books/<int:page>')
-# def show_books(page: int = 1):
-#     return redirect(url_for('books', page=page))
+@app.route('/books/?<isbn>')
+def show_books_by_isbn(isbn):
+    result = {'books': query.search_book_by_isbn(isbn)}
+    return jsonify(result)
+
+
+@app.route('/books/query/<isbn>')
+def get_books_by_isbn(isbn):
+    return redirect(url_for('show_books_by_isbn', isbn=isbn))
+
+
+@app.route('/books/<int:page>')
+def show_books_by_page(page=1):
+    """
+
+    """
+    return redirect(url_for('show_books', page=page))
 
 
 @app.route('/orders/')
 def show_orders():
     """
     show a list of all the orders
+
+    only Administrator permission
+    """
+    return 'hehe'
+
+
+@app.route('/users/')
+def show_users():
+    """
+    show a list of all the users
 
     only Administrator permission
     """
--- server/proc/preproc.py
+++ server/proc/preproc.py
@@ -2,8 +2,8 @@ import datetime
 import decimal
 
 
-BOOK_HEADER = ('ISBN', '书名', '作者', '出版社', '定价',
-               '封面', '类别', '库存', '描述信息', '出版日期')
+BOOK_HEADER = ('isbn', 'name', 'author', 'publisher', 'price',
+               'cover', 'category', 'amount', 'description', 'publish_date')
 ORDER_HEADER = ()
 USER_HEADER = ()
 
@@ -11,8 +11,6 @@ USER_HEADER = ()
 def books_to_dict(records: list):
     """
     transfrom book records into dict
-
-    tips: all the values' type is str
     """
     result = []
     for record in records:
@@ -21,6 +19,7 @@ def books_to_dict(records: list):
             if isinstance(value, datetime.date):
                 record[index] = value.isoformat()  # date trans to str
             elif isinstance(value, decimal.Decimal):
-                record[index] = str(value)  # decimal trans to str
+                record[index] = float(value.quantize(
+                    decimal.Decimal('0.00')))  # decimal trans to str
         result.append(dict(zip(BOOK_HEADER, record)))
     return result
