eba2122f8ecf27d1f5ce416f610f0406ecb643db
lolimay <lolimay@lolimay.cn>
2019-10-23 17:01:25 +0800

    redesign booklist with antd UI lib

--- src/components/BookPreview.js
+++ src/components/BookPreview.js
@@ -2,15 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import { PUBLIC_URL } from '../constants';
 import { Link } from 'react-router-dom';
+import { Card } from 'antd';
+
+const { Meta } = Card;
 
 export default class BookPreview extends React.Component {
+    trimDescription(str) {
+        return str.length > 50 ? `${ str.slice(0, 50) }...` : str;
+    }
+
     render() {
-        const { name, cover } = this.props;
+        const { name, cover, description } = this.props;
 
         return (
             <StyledBookPreview>
-                <Link to='/bookdetail'><img src={ `${ PUBLIC_URL }/${ cover }` }></img></Link>
-                <div className='book-name'><Link to='/bookdetail'>{ name }</Link></div>
+                <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt={ name } src={ cover } />}
+                    className='preview-card'
+                >
+                    <Meta title={ name } description={ this.trimDescription(description) } />
+                </Card>
             </StyledBookPreview>
         );
     }
@@ -18,8 +31,16 @@ export default class BookPreview extends React.Component {
 
 const StyledBookPreview = styled.div`
     display: inline-block;
-    margin: 10px 30px;
+    margin: 10px 0;
     position: relative;
+
+    .preview-card {
+        height: 340px;
+    }
+
+    .ant-card-cover {
+        height: 146.56px;
+    }
 
     img {
         width:100px;
