835c5d0da1cea7cf24d50630db3e77622f35d411
lolimay <lolimay@lolimay.cn>
2019-10-23 16:29:47 +0800

    add NomalLoginForm

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
-export default class Login extends React.Component {
-    render() {
-        return (
-            <StyledLoginPanel>
-                <div>用户名: 
-                    <input type='text' name='username'></input>
-                </div>
-                <div>密码: 
-                    <input type='password' name='password'></input>
-                </div>
-                <button sr></button>
-            </StyledLoginPanel>
-        );
-    }
+class NormalLoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+    );
+  }
 }
 
-const StyledLoginPanel = styled.div`
-    box-shadow: 0 0 0 20px #666666;
-    max-width: 600px;
-    margin: 200px auto;
-    padding: 10px;
-`;
+export default Form.create({ name: 'normal_login' })(NormalLoginForm);
